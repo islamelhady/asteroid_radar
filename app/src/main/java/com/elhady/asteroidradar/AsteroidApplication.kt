b964ed96178ad29175f5259c929f7fc7174d137a
islamelhady <i.elhady44@gmail.com>
2023-01-02 09:29:50 +0200

    cache the data of the asteroid by using a worker, so it downloads and saves today's asteroids in background once a day

--- app/src/main/java/com/elhady/asteroidradar/AsteroidApplication.kt
+++ app/src/main/java/com/elhady/asteroidradar/AsteroidApplication.kt
@@ -1,12 +1,42 @@
 package com.elhady.asteroidradar
 
 import android.app.Application
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
+import com.elhady.asteroidradar.work.RefreshDataWorker
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
-class AsteroidApplication: Application() {
+class AsteroidApplication : Application() {
+
+    private val applicationScope = CoroutineScope(Dispatchers.Default)
 
     override fun onCreate() {
         super.onCreate()
-        Timber.plant(Timber.DebugTree())
+        delayedInit()
+    }
+
+    /**
+     * Setup WorkManager background job to 'fetch' new network data once a day (daily).
+     */
+    private fun setupRecurringWork() {
+        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(1, TimeUnit.DAYS).build()
+        Timber.d("Periodic work request for sync is scheduled")
+        WorkManager.getInstance().enqueueUniquePeriodicWork(
+            RefreshDataWorker.WORK_NAME,
+            ExistingPeriodicWorkPolicy.KEEP,
+            repeatingRequest
+        )
+    }
+
+    fun delayedInit(){
+        applicationScope.launch {
+            Timber.plant(Timber.DebugTree())
+            setupRecurringWork()
+        }
     }
 }
---
+++ app/src/main/java/com/elhady/asteroidradar/work/RefreshDataWorker.kt
@@ -0,0 +1,36 @@
+package com.elhady.asteroidradar.work
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.elhady.asteroidradar.Constants
+import com.elhady.asteroidradar.local.AppDatabase
+import com.elhady.asteroidradar.repository.AsteroidRepository
+import retrofit2.HttpException
+import timber.log.Timber
+
+class RefreshDataWorker(context: Context, params: WorkerParameters) :
+    CoroutineWorker(context, params) {
+
+    override suspend fun doWork(): Result {
+        val database = AppDatabase.getInstance(applicationContext)
+        val repository = AsteroidRepository(database)
+
+        try {
+            repository.getAllAsteroids(
+                Constants.getToday(),
+                Constants.getSevenDaysLater(),
+                Constants.API_KEY
+            )
+            Timber.d("Work request for sync is run")
+        } catch (e: HttpException) {
+            return Result.retry()
+        }
+
+        return Result.success()
+    }
+
+    companion object {
+        const val WORK_NAME = "com.elhady.asteroidradar.work.RefreshDataWorker"
+    }
+}
